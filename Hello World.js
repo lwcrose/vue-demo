4528b3f8d7aaf630a2f702d488685ced1acd5cc7
rose <lwcrose@126.com>
2018-05-07 22:48:06 +0800

    v-bind:id

--- Hello World.js
+++ Hello World.js
@@ -1,6 +1,15 @@
-var app = new Vue({
-    el: '#app',
+var app1 = new Vue({
+    el: '#app-1',
     data:{
         message: 'Hello Vue !' 
+    }
+})
+
+var app2 = new Vue({
+    el: '#app-2',
+    data:{
+        message2: '页面加载于' + new Date().toLocaleString(),
+        message3: 'lex',
+        message4: 'rose'
     }
 })
