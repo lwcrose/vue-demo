c11bd02a4b7c91cb5acdbe0073af41f71309fa52
rose <lwcrose@126.com>
2018-05-09 22:33:20 +0800

    vue-0509

--- Hello World.js
+++ Hello World.js
@@ -8,9 +8,9 @@ var app1 = new Vue({
 var app2 = new Vue({
     el: '#app-2',
     data:{
-        message2: '页面加载于' + new Date().toLocaleString(),
-        message3: 'lex',
-        message4: 'rose'
+        message21: '页面加载于' + new Date().toLocaleString(),
+        message22: 'lex',
+        message23: 'rose'
     }
 })
 
@@ -68,5 +68,62 @@ var app7 = new Vue({
             {id:1, text: 'Milk'},
             {id:2, text: 'Anything Other'}
         ]
+    }
+})
+
+var data1 = {a: 1}
+
+var vm = new Vue({
+    data: data1
+})
+
+var xyz = new Vue({
+    data:{
+        newTodoText: '',
+        visitCount: 0,
+        hideCompletedTodos: false,
+        todo: [],
+        error: null
+    }
+})
+
+
+var obj ={
+    foo: 'bar'
+}
+
+//Object.freeze(obj)
+
+var app8 = new Vue({
+    el: '#app-8',
+    data: obj
+})
+
+new Vue({
+    data: {
+        a:5
+    },
+    created: function(){
+        //‘this’ 指向VM实例
+        console.log('a is: ' + this.a)
+    }
+})
+
+console.log('a is: hahaha')
+
+var app9 = new Vue({
+    el: "#app-9",
+    data:{
+        msg91: 'It could be changed.',
+        msg92: "It won't change.",
+        rawHtml: "<span style=\"color:red\">This should be red.</span>",
+    }
+})
+
+var app10 = new Vue({
+    el: "#app-10",
+    data:{
+        dynamicId: "JIAJIA",
+        isButtonDisabled: true // null/undefined/false ---> <Button>
     }
 })
