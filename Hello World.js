9920d84de83cd1ecc1da2bec3746626284207f40
rose <lwcrose@126.com>
2018-05-10 23:59:28 +0800

    setter

--- Hello World.js
+++ Hello World.js
@@ -1,13 +1,13 @@
 var app1 = new Vue({
     el: '#app-1',
-    data:{
-        message: 'Hello Vue !' 
+    data: {
+        message: 'Hello Vue !'
     }
 })
 
 var app2 = new Vue({
     el: '#app-2',
-    data:{
+    data: {
         message21: '页面加载于' + new Date().toLocaleString(),
         message22: 'lex',
         message23: 'rose'
@@ -15,44 +15,44 @@ var app2 = new Vue({
 })
 
 var app3 = new Vue({
-    el:'#app-3',
-    data:{
-        see:true
+    el: '#app-3',
+    data: {
+        see: true
     }
 })
 
 var app4 = new Vue({
     el: '#app-4',
-    data:{
-        todos:[
-            {textbbb: '学习 JavaScripts'},
-            {textbbb: '学习 Vue'},
-            {text: '整个牛逼项目'}
+    data: {
+        todos: [
+            { textbbb: '学习 JavaScripts' },
+            { textbbb: '学习 Vue' },
+            { text: '整个牛逼项目' }
         ]
     }
 })
 
 var app5 = new Vue({
     el: '#app-5',
-    data:{
+    data: {
         message5: "Hello Vue.js !",
         message52: "Hello",
         message53: "Dear Lex",
         message54: "Hi Rose"
     },
-    methods:{
-        reversedMessage5: function(){
+    methods: {
+        reversedMessage5: function () {
             this.message5 = this.message5.split('').reverse().join('')
         },
-        reversedMessage53: function(){
-            return  this.message53.split('').reverse().join('')   
-        }    
+        reversedMessage53: function () {
+            return this.message53.split('').reverse().join('')
+        }
     },
-    computed:{
-        reversedMessage52: function(){
+    computed: {
+        reversedMessage52: function () {
             return this.message52.split('').reverse().join('')
         },
-        reversedMessage54: function(){
+        reversedMessage54: function () {
             return this.message54.split('').reverse().join('')
         }
     },
@@ -66,13 +66,13 @@ var app5 = new Vue({
 
 var app6 = new Vue({
     el: '#app-6',
-    data:{
+    data: {
         message6: "Hello Vue6 !",
         message62: "Hello Vue6-2 !"
     }
 })
 
-Vue.component('todo-item',{
+Vue.component('todo-item', {
     //todo-item 组件现在接受一个 “prop”，类似于一个自定义特性。
     //这个 prop 名为 todoprop
     props: ['todoprop'],
@@ -82,23 +82,23 @@ Vue.component('todo-item',{
 
 var app7 = new Vue({
     el: '#app-7',
-    data:{
-        groceryList:[
-            {id:0, text: 'Vegetable'},
-            {id:1, text: 'Milk'},
-            {id:2, text: 'Anything Other'}
+    data: {
+        groceryList: [
+            { id: 0, text: 'Vegetable' },
+            { id: 1, text: 'Milk' },
+            { id: 2, text: 'Anything Other' }
         ]
     }
 })
 
-var data1 = {a: 1}
+var data1 = { a: 1 }
 
 var vm = new Vue({
     data: data1
 })
 
 var xyz = new Vue({
-    data:{
+    data: {
         newTodoText: '',
         visitCount: 0,
         hideCompletedTodos: false,
@@ -108,7 +108,7 @@ var xyz = new Vue({
 })
 
 
-var obj ={
+var obj = {
     foo: 'bar'
 }
 
@@ -121,9 +121,9 @@ var app8 = new Vue({
 
 new Vue({
     data: {
-        a:5
+        a: 5
     },
-    created: function(){
+    created: function () {
         //‘this’ 指向VM实例
         console.log('a is: ' + this.a)
     }
@@ -133,7 +133,7 @@ console.log('a is: hahaha')
 
 var app9 = new Vue({
     el: "#app-9",
-    data:{
+    data: {
         msg91: 'It could be changed.',
         msg92: "It won't change.",
         rawHtml: "<span style=\"color:red\">This should be red.</span>",
@@ -142,9 +142,37 @@ var app9 = new Vue({
 
 var app10 = new Vue({
     el: "#app-10",
-    data:{
+    data: {
         dynamicId: "JIAJIA",
         isButtonDisabled: true // null/undefined/false ---> disabled 特性不会被包含在渲染出来的<Button>元素中
     }
 })
 // zhushi
+
+var demo1 = new Vue({
+    el: "#demo-1",
+    data: {
+        firstName: "Luo",
+        lastName: "Rose",
+        //newValue: null
+    },
+    // computed:{
+    //     fullName: function(){
+    //         return this.firstName + ' ' + this.lastName
+    //     }
+    // }
+    computed: {
+        fullName: {
+            // getter
+            get: function () {
+                return this.firstName + ' ' + this.lastName
+            },
+            //setter
+            set: function (newValue) {
+                var names = newValue.split(' ')
+                this.firstName = names[0]
+                this.lastName = names[names.length - 1]
+            }
+        }
+    }
+})
